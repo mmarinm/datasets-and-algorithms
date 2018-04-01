784f3ff095a4805fbdd63ed4c0ff7e1608aae9b2
mmarinm <mestrovic.ma@gmail.com>
2018-03-31 20:58:38 -0400

    ""

--- generators.js
+++ generators.js
@@ -1,9 +1,9 @@
-function * EventGenerator() {
+function* EventGenerator() {
     let num = 2;
-     while (true) {
-         yield num
-         num = num + 2;
-     }
+    while (true) {
+        yield num;
+        num = num + 2;
+    }
 }
 
 let iterator = EventGenerator();
@@ -11,3 +11,16 @@ let a1 = iterator.next().value;
 let a2 = iterator.next().value;
 let a3 = EventGenerator().next().value;
 let a4 = iterator.next().value;
+
+function* NinjaGenerator() {
+    yield "Yoshi";
+    return "Hatori";
+    yield "Hanzo";
+}
+
+const ninjas = []
+for (let ninja of NinjaGenerator()) {
+    ninjas.push(ninja)
+}
+
+console.log(ninjas)
